b94e13ba5313fc71dca10e799d8f86e9f8be9fd8
biaar <biaar@10.149.211.65>
2025-04-04 19:22:57 -0300

    testando git

---
+++ src/main/java/com/mycompany/teste1/Teste1.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.teste1;
+
+import javax.swing.JOptionPane;
+
+public class Teste1 {
+
+     public static void main(String[] args) {
+    String nome;
+    nome = JOptionPane.showInputDialog("Digite seu nome:");
+}
+}
